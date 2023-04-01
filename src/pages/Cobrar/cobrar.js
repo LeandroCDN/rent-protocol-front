6faa8d9f9e92ce303a4a5b2c5151ea080dc6a50c
Lauta Hern <lautaa.94@gmail.com>
2023-04-01 15:14:06 -0300

    testing blockchain

--- src/pages/Cobrar/cobrar.js
+++ src/pages/Cobrar/cobrar.js
@@ -1,21 +1,68 @@
 import './cobrar.css';
 import ima from './Ellipse1.png';
+import { ethers } from 'ethers';
+import rentAbi from "../../Contracts/rentABI.json";
+import React, { useEffect, useState } from 'react';
 
-import React, { useState } from 'react';
+const rentAddress = "0xD6D59e9f8BEe9919dba3261aE9FaEDFDD6A6764a";
+
 
 function Cobrar() {
   const [showForm, setShowForm] = useState(false);
-  const [property, setProperty] = useState('');
+  const [id, setId] = useState('');
+  const [rentContract, setRentContract] = useState('');
+  const [isConnected, setIsConnected] = useState(false);
+  
   const handleClick = () => {
     setShowForm(true);
   };
+
+  const createSigner = async () => {
+    if (window.ethereum ) {
+      await window.ethereum.request({ method: 'eth_requestAccounts' });
+      if (window.ethereum.selectedAddress) {
+        setIsConnected(true);
+        initContract();
+      }
+    }
+  };
+
+  //utilizamos esto para verificar la coneccion con metamastk
+  useEffect(() => {
+    createSigner();
+    //si tiene wallet
+    if (window.ethereum ) {
+      //registra constantemente cambios en el objeto 'cuenta'
+      window.ethereum.on('accountsChanged', (accounts) => {
+        if (accounts.length > 0) {
+          setIsConnected(true);
+        } else {
+          setIsConnected(false);
+        }
+      });
+    } 
+  }, []);
+
+  const initContract = async () => {
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    const signer = provider.getSigner();
+    const rentContract = new ethers.Contract(rentAddress, rentAbi, signer);
+    setRentContract(rentContract);
+  }
+
+  const getDataBlockchain = async (event)=> {
+    initContract();
+    event.preventDefault();
+    const seeAmount = await rentContract.calculatePayment(0);
+    console.log(seeAmount);
+  }
 
   return (
     <div className='back'>
       <div className='divv'>
         <img className='divvv marge' src={ ima }  alt='casita'/>
-      <input type='input' className='entrada' value={property} onChange={(event) => setProperty(event.target.value)} placeholder='ID Propiedad' />
-      <button className='boton'  onClick={handleClick} disabled= {!property} >Mostrar Formulario</button>
+      <input type='input' className='entrada' value={id} onChange={(event) => setId(event.target.value)} placeholder='ID Propiedad' />
+      <button className='boton'  onClick={getDataBlockchain} disabled= {!id} >Mostrar Formulario</button>
       </div>
       {showForm ? (
         <form className='formu'>
