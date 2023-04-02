cd1d6eb671714fec94f7dfd707da3ba41ee5c53f
PatoVerde <leandrodenos@gmail.com>
2023-04-02 00:17:31 -0300

    nav foot update

--- src/Components/Footer/index.js
+++ src/Components/Footer/index.js
@@ -10,11 +10,15 @@ function Footer() {
         <a href="/"  rel="noopener noreferrer"> DevFuri </a>
         {/* <a href="https://www.facebook.com/" target="_blank" rel="noopener noreferrer"><i className="fab fa-facebook-f"></i></a>
         <a href="https://www.instagram.com/" target="_blank" rel="noopener noreferrer"><i className="fab fa-instagram"></i></a>
-        <a href="https://www.linkedin.com/" target="_blank" rel="noopener noreferrer"><i className="fab fa-linkedin-in"></i></a> */}
+      <a href="https://www.linkedin.com/" target="_blank" rel="noopener noreferrer"><i className="fab fa-linkedin-in"></i></a> */}
       </div>
       <div className="useful-links">
+        <a href="https://mumbai.polygonscan.com/address/0xD6D59e9f8BEe9919dba3261aE9FaEDFDD6A6764a#writeContract"  rel="noopener noreferrer"> Mint Mock Token </a>
+        <a className="aSpace">|</a>
         <a href="https://duckdev.notion.site/Solidity-Documentaci-n-924df1f1536d49028a075e487ebe496b" target="_blank" rel="noopener noreferrer">Documentation</a>
+        <a className="aSpace">|</a>
         <a href="https://duckdev.notion.site/King-of-dev-2-0-Team-DevFury-a79221d7f1bc47d0a4c1bae651c57014" target="_blank" rel="noopener noreferrer">Team Members</a>
+        <a className="aSpace">|</a>
         <a href="https://duckdev.notion.site/Project-track-Hackaton-workshit-072ffce2ef1d4b3ea20a22f3952fdffb" target="_blank" rel="noopener noreferrer">Project Info</a>
       </div>
     </div>
